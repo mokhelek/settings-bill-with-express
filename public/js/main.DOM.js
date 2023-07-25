7778dc6f5ab7b2fd42376c69e64d8a48495725b8
mokhelek <mkatleho191@gmail.com>
2023-07-25 15:13:35 +0200

    fixed error with billtype validator

--- public/js/main.DOM.js
+++ public/js/main.DOM.js
@@ -1,52 +1,44 @@
 let inputError = document.querySelector(".input-error");
 let billTypeError = document.querySelector(".bill-type-input-error");
 
-let formElem = document.querySelector(".costs-form")
-let billTypeFormElem = document.querySelector(".bill-type-form")
+let formElem = document.querySelector(".costs-form");
+let billTypeFormElem = document.querySelector(".bill-type-form");
 
 let callCostInputElem = document.querySelector(".callCostSetting");
 let smsCostInputElem = document.querySelector(".smsCostSetting");
 let warningLevelElem = document.querySelector(".warningLevelSetting");
 let criticalLevelElem = document.querySelector(".criticalLevelSetting");
 
-
-let radioBtn = document.querySelector(".billItemTypeWithSettings").checked;
-
-document.addEventListener("DOMContentLoaded",()=>{
-
-    formElem.addEventListener("submit",(event)=>{
+document.addEventListener("DOMContentLoaded", () => {
+    formElem.addEventListener("submit", (event) => {
         event.preventDefault();
 
-
         if (callCostInputElem.value && smsCostInputElem.value && warningLevelElem.value && criticalLevelElem.value) {
-            formElem.submit()
+            formElem.submit();
         } else {
-            inputError.style.display = "block"
-    
-            setTimeout(()=>{
-                inputError.style.display = "none"
-            },4000)
-        }
+            inputError.style.display = "block";
 
-    })
+            setTimeout(() => {
+                inputError.style.display = "none";
+            }, 4000);
+        }
+    });
 
     // **************************** Bill Type Error ************************
 
-    billTypeFormElem.addEventListener("submit",(event)=>{
+    billTypeFormElem.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        console.log(radioBtn)
+        let radioBtn = document.querySelector(".billItemTypeWithSettings:checked");
 
         if (radioBtn) {
-            formElem.submit()
+            billTypeFormElem.submit();
         } else {
-            billTypeError.style.display = "block"
-    
-            setTimeout(()=>{
-                billTypeError.style.display = "none"
-            },4000)
+            billTypeError.style.display = "block";
+
+            setTimeout(() => {
+                billTypeError.style.display = "none";
+            }, 4000);
         }
-
-    })
-
-})
+    });
+});
