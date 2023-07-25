b88ec47ab29859c4c693fdc0d5b38a0896eb9853
mokhelek <mkatleho191@gmail.com>
2023-07-25 16:53:36 +0200

    allow decimals

--- public/js/settings-bill.js
+++ public/js/settings-bill.js
@@ -101,12 +101,12 @@ export default function SettingsBill() {
     }
 
     function totals() {
-        let smsTotal = getTotal("sms");
-        let callTotal = getTotal("call");
+        let smsTotal = getTotal("sms").toFixed(2);
+        let callTotal = getTotal("call").toFixed(2)  ;
         return {
             callTotal,
             smsTotal,
-            grandTotal: grandTotal(),
+            grandTotal: grandTotal().toFixed(2),
         };
     }
 
